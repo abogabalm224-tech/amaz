1ff502b437bac5866278e7ffb275a299ce39c8ed
abogabalm224-tech <abogabalm224@gmail.com>
2026-05-27 01:44:15 +0300

    Update image_processor.py

--- image_processor.py
+++ image_processor.py
@@ -4,8 +4,8 @@ from PIL import Image
 def apply_frame(screenshot_path, output_path="framed_output.png"):
     frame_path = "frame.png"
 
-    LEFT = 40
-    TOP = 75
+    LEFT = 15
+    TOP = 35
     WIDTH = 1278
     HEIGHT = 775
 
